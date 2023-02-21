bf1a73c928f36e71c5fab63ce9b98073cdd2c2ce
arianecrestani <ariane.mrv@gmail.com>
2023-02-21 17:46:43 +0100

    create a event listener for the buttons paramenters

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const getCountryApi = async () => {
   const response = await fetch(`https://restcountries.com/v3.1/all`);
   const data = await response.json();
-  console.log(data)
+  console.log(data);
   showFlag(data);
-  showName(data)
+  
 };
 getCountryApi();
 
 const showFlag = (data) => {
-  const imageFlag = document.getElementById("flag");
-  imageFlag.setAttribute("src", data[0].flags.png);
 
+  const nameFlag = data[0].flags.png
+  const flagCountryName = data[0].name.common;
+  const imageFlag = document.getElementById("flag");
+  imageFlag.setAttribute("src", nameFlag);
+  showNameCountry(data, flagCountryName);
 };
 
-const showName = (data) => {
+const showNameCountry = (data,flagCountryName) => {
 
-  const firstOption = document.getElementById('firstOption') // put functionallity event listner in this button
-  firstOption.innerHTML = data[0].name.common;
+  // const nameCountry =  data[0].name.common;
+  const firstOption = document.getElementById("firstOption");
+  firstOption.addEventListener("click", function (e) {
+    console.log('e.target.name', e.target.name)
 
-  const secondOption = document.getElementById('secondOption')
+    console.log("User has clicked on the button!");
+    if(flagCountryName === e.target.name) 
+   console.log("correct")
+  });
+  firstOption.innerHTML = nameCountry;
+
+  const secondOption = document.getElementById("secondOption");
   secondOption.innerHTML = data[1].name.common;
 
-  const thirdOption = document.getElementById('thirdOption')
-  thirdOption.innerHTML = data[2].name.common; 
+  const thirdOption = document.getElementById("thirdOption");
+  thirdOption.innerHTML = data[2].name.common;
 
-  const fourthOption = document.getElementById('fourthOption')
+  const fourthOption = document.getElementById("fourthOption");
   fourthOption.innerHTML = data[3].name.common;
-}
-
+};
