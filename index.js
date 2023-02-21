b42dca73e3623719100e3f3c7b34ff35e4563b07
arianecrestani <ariane.mrv@gmail.com>
2023-02-21 15:51:06 +0100

    create a function that shows the flag

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const getCountryApi = async () => {
   const response = await fetch(`https://restcountries.com/v3.1/all`);
   const data = await response.json();
-  console.log(data);
 
-
-  const figure = document.getElementById("flag-container");
-  const imageFlag = document.getElementById("flag");
-  console.log(imageFlag)
-  imageFlag.setAttribute("src", data[1].flags.png);
-  figure.appendChild(imageFlag);
-  console.log(figure)
-
-  return data;
+  showFlag(data);
 };
 getCountryApi();
+
+const showFlag = (data) => {
+  const imageFlag = document.getElementById("flag");
+  imageFlag.setAttribute("src", data[0].flags.png);
+};
