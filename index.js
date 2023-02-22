ec19bace5382d9144fc5128e933144d633d2443d
arianecrestani <ariane.mrv@gmail.com>
2023-02-22 13:55:56 +0100

    created a for loop for show names

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
 const getCountryApi = async () => {
   const response = await fetch(`https://restcountries.com/v3.1/all`);
   const data = await response.json();
-  console.log(data);
-  showFlag(data);
-  
+  showFlagData(data);
 };
 getCountryApi();
 
-const showFlag = (data) => {
-
-  const nameFlag = data[0].flags.png
-  const flagCountryName = data[0].name.common;
+const showFlagData = (data) => {
+  const nameFlag = data[0].flags.png; //show Flag
+  // const flagCountryName = data[i].name.common; // show country name
   const imageFlag = document.getElementById("flag");
   imageFlag.setAttribute("src", nameFlag);
-  showNameCountry(data, flagCountryName);
+  showNameCountry(data);
 };
 
-const showNameCountry = (data,flagCountryName) => {
+const showNameCountry = (data) => {
+  const optionAnswers = document.getElementById("optionAnswers");
 
+  let array = data;
+  let newArray = [];
+
+  console.log(array);
+
+  for (let i = 0; 7 > i; i++) {
+    newArray.push(array[i]);
+    console.log(newArray);
+    const btnAnswer = document.createElement("button");
+    btnAnswer.innerHTML = newArray[i].name.common;
+    optionAnswers.appendChild(btnAnswer);
+  }
+  return newArray;
+
+  // return nameCountry
   // const nameCountry =  data[0].name.common;
-  const firstOption = document.getElementById("firstOption");
-  firstOption.addEventListener("click", function (e) {
-    console.log('e.target.name', e.target.name)
+  // const optionAnswers = document.getElementById("optionAnswers"); // first button
+  // optionAnswers.addEventListener("click", function (e) {
 
-    console.log("User has clicked on the button!");
-    if(flagCountryName === e.target.name) 
-   console.log("correct")
-  });
-  firstOption.innerHTML = nameCountry;
-
-  const secondOption = document.getElementById("secondOption");
-  secondOption.innerHTML = data[1].name.common;
-
-  const thirdOption = document.getElementById("thirdOption");
-  thirdOption.innerHTML = data[2].name.common;
-
-  const fourthOption = document.getElementById("fourthOption");
-  fourthOption.innerHTML = data[3].name.common;
+  //   console.log('e.target.name', e.target.name)
+  //   console.log("User has clicked on the button!");
+  //   if(flagCountryName === e.target.name)
+  //  console.log("correct")
+  // });
+  // buttons.innerHTML = nameCountry;
 };
