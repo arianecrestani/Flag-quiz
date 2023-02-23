a8b5448b38ba84dc37ca36172df1ed312b56e96e
arianecrestani <ariane.mrv@gmail.com>
2023-02-23 15:19:43 +0100

    created answer country

--- index.js
+++ index.js
@@ -1,50 +1,74 @@
 const getCountryApi = async () => {
-
   const response = await fetch(`https://restcountries.com/v3.1/all`);
   const data = await response.json(); // array
-
-  const showRandom = pickRandom(data) // obj
-  showFlagData(showRandom)
-  showRandomCountries(data);
-
+  console.log(data);
+  controller(data);
 };
-getCountryApi()
+getCountryApi();
+
+const controller = (data) => {
+  const showRandom = pickRandom(data); // obj
+  showFlagData(showRandom);
+  showRandomCountries(data, showRandom);
+};
 
 const showFlagData = (data) => {
-
-  const nameFlag = data.flags.png;//show Flag
+  const nameFlag = data.flags.png; //show Flag
   const imageFlag = document.getElementById("flag");
   imageFlag.setAttribute("src", nameFlag);
-  console.log(data)
 };
 // showFlagData(pickRandom(data))
 
-const showRandomCountries = (country) => {
-
+const showRandomCountries = (country, answerCountry) => {
   const country1 = pickRandom(country);
-  showNameCountry(country1);
+  showNameCountry(country1, answerCountry);
+  // console.log(country1);
 
   const country2 = pickRandom(country);
-  showNameCountry(country2);
+  showNameCountry(country2, answerCountry);
+  // console.log(country2);
 
   const country3 = pickRandom(country);
-  showNameCountry(country3);
-
+  showNameCountry(country3, answerCountry);
+  // console.log(country3);
   const country4 = pickRandom(country);
-  showNameCountry(country4);
+  showNameCountry(country4, answerCountry);
+  // console.log(country4);
 
-  return country
+  showNameCountry(answerCountry,answerCountry)
 
 };
 
-const pickRandom = (countryList) => { //array
+const pickRandom = (countryList) => {
+  //array
   return countryList[Math.floor(Math.random() * countryList.length)];
 };
 
-const showNameCountry = (data) => {
-  const optionAnswers = document.getElementById("optionAnswers");
+const showNameCountry = (data, answerCountry) => {
   
+  const optionAnswers = document.getElementById("optionAnswers");
+  console.log(data, answerCountry);
+  console.log(optionAnswers)
+
+  const correctAnswer = answerCountry.name.common;
+  const flagCountryName = data.name.common;
+
   const btnAnswer = document.createElement("button");
-  btnAnswer.innerHTML = data.name.common;
+  btnAnswer.innerHTML = flagCountryName;
+  btnAnswer.setAttribute("name", flagCountryName);
   optionAnswers.appendChild(btnAnswer);
+  console.log();
+
+  btnAnswer.addEventListener("click", function (e) {
+    console.log("e.target.name", e.target.name);
+    console.log("flagCountryName", flagCountryName);
+
+    if (e.target.name === correctAnswer) {
+      btnAnswer.setAttribute("style", "background-color:green");
+      console.log("correct", e.target.name);
+    }else {
+      btnAnswer.setAttribute("style", "background-color:red");
+      console.log("incorrect");
+    }
+  });
 };
