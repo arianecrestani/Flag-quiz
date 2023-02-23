141c9312b1f7bed95fba4c1b7f155d3635a957c9
arianecrestani <ariane.mrv@gmail.com>
2023-02-23 09:57:09 +0100

    refactored

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
 const getCountryApi = async () => {
+
   const response = await fetch(`https://restcountries.com/v3.1/all`);
-  const data = await response.json();
-  showFlagData(data);
+  const data = await response.json(); // array
+
+  const showRandom = pickRandom(data) // obj
+  showFlagData(showRandom)
+  showRandomCountries(data);
+
 };
-getCountryApi();
+getCountryApi()
 
 const showFlagData = (data) => {
-  const nameFlag = data[0].flags.png; //show Flag
+
+  const nameFlag = data.flags.png;//show Flag
   const imageFlag = document.getElementById("flag");
   imageFlag.setAttribute("src", nameFlag);
-  showNameCountry(data);
+  console.log(data)
+};
+// showFlagData(pickRandom(data))
+
+const showRandomCountries = (country) => {
+
+  const country1 = pickRandom(country);
+  showNameCountry(country1);
+
+  const country2 = pickRandom(country);
+  showNameCountry(country2);
+
+  const country3 = pickRandom(country);
+  showNameCountry(country3);
+
+  const country4 = pickRandom(country);
+  showNameCountry(country4);
+
+  return country
+
 };
 
-function pickRandom(data) {
-  let randomArray = data;
-  return randomArray[Math.floor(Math.random() * randomArray.length)];
-}
+const pickRandom = (countryList) => { //array
+  return countryList[Math.floor(Math.random() * countryList.length)];
+};
 
 const showNameCountry = (data) => {
   const optionAnswers = document.getElementById("optionAnswers");
-
-  let array = data;
-  let newArray = [];
-  console.log(data);
-
-  for (let i = 0; 5 >= i; i++) {
-    newArray.push(array[i]);
-
-    const btnAnswer = document.createElement("button");
-    let newArrayOfCountryNames = array[i].name.common;
-    btnAnswer.innerHTML = newArrayOfCountryNames;
-    optionAnswers.appendChild(btnAnswer);
-
-    btnAnswer.addEventListener("click", function (e) {
-    console.log("e.target.name", e.target.name);
-    console.log("User has clicked on the button!");
-    if (newArrayOfCountryNames === e.target.name) console.log("correct");
-    });
-  }
-  // return newArray
-
-  // return nameCountry
-  // const nameCountry =  data[0].name.common;
-  // const optionAnswers = document.getElementById("optionAnswers"); // first button
-
-  // buttons.innerHTML = nameCountry;
-  // return  newArray
+  
+  const btnAnswer = document.createElement("button");
+  btnAnswer.innerHTML = data.name.common;
+  optionAnswers.appendChild(btnAnswer);
 };
